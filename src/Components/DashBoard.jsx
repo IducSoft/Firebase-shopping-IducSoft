c04ab8018a318c31a34065b5fd0d58efad7ce454
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2022-12-17 14:15:48 -0400

    ahora vamos a probrar el resto

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -2,16 +2,17 @@ import React,{ useContext } from 'react';
 import {AppContext} from "../App.js"
 
 const DashBoard = () => {
-  const [state, dispatch] = useContext(AppContext);
+  const [state] = useContext(AppContext);
 
   return (
-    <div className='mt-20'>
+    <div className='mt-20 bg-violet-300 px-4 py-3'>
       
-      <div className='w-full'>
+      <div className='w-full text-left'>
+      <h1>welcome to our App </h1>
         <span>
-          <img src={state.user.photoURL} alt="img-profile"/>
           <h1>{state.user.displayName}</h1>
         </span>
+        {/* <img src={state.user.photoURL} alt='Img-profile'/> */}
       </div>
     </div>
   )
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React,{ useContext } from 'react';
+import { Link} from "react-router-dom";
 import {AppContext} from "../App.js"
 import { AiOutlineHome } from "react-icons/ai";
 import { AiFillAccountBook } from "react-icons/ai";
@@ -9,18 +10,17 @@ const Footer = () => {
     const [state] = useContext(AppContext);
   return (
     <div className='bg-pink fixed bottom-0 w-full py-3 px-5 bg-purple-900 text-white'>
-
         <div className='flex justify-center mb-4'>
-            <span className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'> <AiOutlineHome/></span>
+            <Link to="/" className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'>  <AiOutlineHome/></Link>
+            {/* <span className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'> <AiOutlineHome/></span> */}
             {
-                state.isLogin === true && (<span className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'><AiOutlineBars/> </span>)
+                state.isLogin === true && (<Link to="dashboard" className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'> <AiOutlineBars/></Link>)
             }
             <span className='mx-3 text-black bg-white text-4xl p-2 rounded-full cursor-pointer'><AiFillAccountBook/> </span>
             
             
         </div>
         <span>This project is made by <a href='https://github.com/IducSoft' className='text-black' target="_blank" rel="noreferrer">Isaac Urdaneta</a></span>
-
     </div>
   )
 }
